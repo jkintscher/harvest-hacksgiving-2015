7e357ae553e10ffb590b16ca18105739423df419
Joschka <joschka@kintscher.me>
2015-11-24 22:32:15 -0500

    Serve rendered lunch card

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const http = require('http');
 const BOARD_ID = '56535fa5c071b19aab2ad5b6';
 
 let card = {};
@@ -8,4 +9,17 @@ let card = {};
 require('./lib/board').load(BOARD_ID, (board) =>
   board.getRandomCard((data) => card = data)
 );
+
+// Serve it up
+const server = http.createServer((req, res) => {
+  if (req.url !== '/') {
+    res.statusCode = 404;
+    res.statusMessage = 'Not found';
+    res.end();
+  }
+
+  res.end(require('./templates/index')(card));
+});
+
+server.listen(8080);
 
