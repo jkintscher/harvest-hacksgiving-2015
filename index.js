716521ff8e1345960d80b3b29ff514b149769d6c
Joschka <joschka@kintscher.me>
2015-11-25 12:09:09 -0500

    On Wednesdays, select team lunch card instead

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
 const http = require('http');
 const BOARD_ID = '56535fa5c071b19aab2ad5b6';
 
-const WEDNESDAY = 3;
 let card = {};
-let is_team_lunch = new Date().getDay() === WEDNESDAY;
+let is_team_lunch = (new Date().getDay() === 3);
 
 // Randomly select today’s lunch card
-require('./lib/board').load(BOARD_ID, (board) =>
-  board.getRandomCard((data) => card = data)
-);
+require('./lib/board').load(BOARD_ID, (board) => {
+  if (is_team_lunch) {
+    board.getRandomTeamLunchCard((data) => card = data)
+  } else {
+    board.getRandomCard((data) => card = data)
+  }
+});
 
-// Serve it up
+// Serve it
 const server = http.createServer((req, res) => {
   if (req.url !== '/') {
     res.statusCode = 404;
